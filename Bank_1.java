8e1cbb826a5e9e140e1abddddb1a23d10a264aba
Sankarshan S <110289140+Sankarshans07@users.noreply.github.com>
2022-08-04 07:27:04 +0530

    OOPS

---
+++ Bank_1.java
@@ -0,0 +1,17 @@
+package oops;
+
+public class Bank_1 {
+
+	public static  void carLoan() {
+		System.out.println("Car Loan");
+	}
+	
+	public static void fixedDeposit() {
+		System.out.println("fixedDeposit");
+	}
+	
+	public static void main(String[] args){  
+		Bank_1.carLoan();
+		Bank_1.fixedDeposit();
+	}
+}
